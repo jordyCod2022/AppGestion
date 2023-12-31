69592ea99fdfd0c6b816e6bdda4d63a277699fa8
Jordy Bernabe <bernabejoseph79@gmail.com>
2023-12-31 17:43:21 -0500

    0000

--- index.js
+++ index.js
@@ -45,6 +45,36 @@ app.get("/", (req, res) => {
   res.sendFile(indexPath);
 });
 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Realizar un JOIN entre las tablas para obtener la información del colaborador y el token
+    const result = await pool.query(
+      'SELECT c.id_colaborador, c.cedula, t.token FROM public.colaboradores c JOIN public.httptoken t ON c.id_colaborador = t.id_colaboradorfk WHERE c.cedula = $1',
+      [username]
+    );
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      // Aquí puedes realizar la validación de la contraseña o cualquier otra lógica que necesites
+      // También puedes verificar el token si es necesario
+      if (password_valida) {
+        // Aquí puedes generar un token de sesión, establecer cookies, etc.
+        res.send('Inicio de sesión exitoso');
+      } else {
+        res.status(401).send('Credenciales incorrectas');
+      }
+    } else {
+      res.status(401).send('Usuario no encontrado');
+    }
+  } catch (error) {
+    console.error('Error en el inicio de sesión:', error);
+    res.status(500).send('Error en el servidor');
+  }
+});
+
+
 const PORT = 3000;
 
 const server = app.listen(PORT, () => {
