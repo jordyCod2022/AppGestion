e3293af3639d7af25d8321bdac781b2f77431afb
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-01-01 17:37:54 -0500

    0000

--- public/login.js
+++ public/login.js
@@ -9,25 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const authenticated = await attemptLogin(username, password);
 
-    // ...
+    if (authenticated) {
+      alert('Inicio de sesión exitoso');
+    
+      // Realizar automáticamente la llamada a /getNombre después del inicio de sesión exitoso
+      const nombreResponse = await fetch('/getNombre?username=' + username);
+      const nombreData = await nombreResponse.json();
+    
+      // Almacena los datos en localStorage
+      localStorage.setItem('nombreData', JSON.stringify(nombreData));
+    
+      // Redirigir o realizar acciones adicionales después del inicio de sesión exitoso
+      window.location.href = '/protected/dashboard/dashboard.html';
+    } else {
+      alert('Nombre de usuario o contraseña incorrectos');
+    }
 
-if (authenticated) {
-  alert('Inicio de sesión exitoso');
 
-  // Realizar automáticamente la llamada a /getNombre después del inicio de sesión exitoso
-  const nombreResponse = await fetch('/getNombre?username=' + username);
-  const nombreData = await nombreResponse.json();
-
-  // Almacena los datos en localStorage
-  localStorage.setItem('nombreData', JSON.stringify(nombreData));
-
-  // Redirigir o realizar acciones adicionales después del inicio de sesión exitoso
-  window.location.href = '/protected/dashboard/dashboard.html';
-} else {
-  alert('Nombre de usuario o contraseña incorrectos');
-}
-// ...
-
+  });
 
   async function attemptLogin(username, password) {
     const response = await fetch('/login', {
