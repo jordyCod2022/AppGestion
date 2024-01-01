732fbfd8701aabbdefd0ad12c67e92c6d0859942
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-01-01 08:39:43 -0500

    0000

--- public/protected/dashboard/dashboard.js
+++ public/protected/dashboard/dashboard.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    // Hacer una solicitud al backend para obtener el nombre del usuario
-    const response = await fetch('/getNombre');
-    const data = await response.json();
-
-    // Actualizar el contenido del elemento con el nuevo nombre
-    const userWelcomeElement = document.querySelector('.user-welcome span');
-    userWelcomeElement.textContent = `Bienvenido, ${data.nombre || 'Usuario'}`;
+    try {
+      // Hacer una solicitud al backend para obtener el nombre del usuario
+      const response = await fetch('/getNombre');
+      const data = await response.json();
+  
+      // Verificar la respuesta completa en la consola
+      console.log('Respuesta del servidor:', data);
+  
+      // Actualizar el contenido del elemento con el nuevo nombre
+      const userWelcomeElement = document.querySelector('.user-welcome span');
+      userWelcomeElement.textContent = `Bienvenido, ${data.nombre || 'Usuario'}`;
+    } catch (error) {
+      console.error('Error al obtener el nombre del usuario:', error);
+    }
   });
+  
