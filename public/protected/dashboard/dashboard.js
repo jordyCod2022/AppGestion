3670dbf8dbd5171cc79c8402e02a4ff183955524
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-01-01 17:45:17 -0500

    0000

--- public/protected/dashboard/dashboard.js
+++ public/protected/dashboard/dashboard.js
@@ -10,4 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     console.log('No hay datos disponibles en el dashboard');
   }
+
+  // Agrega la funcionalidad al botón de cerrar sesión
+  const logoutButton = document.querySelector('.logout-button');
+  if (logoutButton) {
+    logoutButton.addEventListener('click', () => {
+      const confirmLogout = confirm('¿Estás seguro de cerrar sesión?');
+      if (confirmLogout) {
+        // Limpiar datos de localStorage y redirigir a la página de inicio de sesión
+        localStorage.removeItem('nombreData');
+        window.location.replace('/login.html'); // Redirige y reemplaza la entrada en el historial
+      }
+    });
+  } else {
+    console.log('No se encontró el botón de cerrar sesión');
+  }
 });
