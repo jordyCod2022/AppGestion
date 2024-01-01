f160dce9a0d7bdb7c933d3a1b7024c6a304edeb1
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-01-01 07:03:34 -0500

    0000

--- public/login.js
+++ public/login.js
@@ -6,41 +6,36 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
-  
-    loginForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
-  
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-  
-      const authenticated = await attemptLogin(username, password);
-  
-      if (authenticated) {
-        alert('Inicio de sesión exitoso');
-        const rutaArchivo = path.join(__dirname, 'protected', 'dashboard', 'dashboard.html');
-        return res.redirect(rutaArchivo);
+  const loginForm = document.getElementById('loginForm');
 
-  
-        // Redirigir o realizar acciones adicionales después del inicio de sesión exitoso
-      } else {
-        alert('Nombre de usuario o contraseña incorrectos');
-      }
-    });
-  
-    async function attemptLogin(username, password) {
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-  
-      const data = await response.json();
-      return data.authenticated;
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const authenticated = await attemptLogin(username, password);
+
+    if (authenticated) {
+      alert('Inicio de sesión exitoso');
+      window.location.href = '/protected/dashboard/dashboard.html';
+      // Redirigir o realizar acciones adicionales después del inicio de sesión exitoso
+    } else {
+      alert('Nombre de usuario o contraseña incorrectos');
     }
   });
-  
+
+  async function attemptLogin(username, password) {
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+    return data.authenticated;
+  }
+});
