c58e057db3e22f04dbaf90aebef82c001dd82676
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-01-11 20:09:59 -0500

    nuevo

--- index.js
+++ index.js
@@ -49,11 +49,10 @@ process.on('SIGINT', () => {
 app.use(express.static('public'));
 app.use(bodyParser.json());  // Necesitas agregar este middleware para manejar el cuerpo de la solicitud JSON
 
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const absolutePath = path.join(__dirname, 'uploads'); // Ruta absoluta
-    cb(null, absolutePath);
+    // Corregir aquí el nombre de la función, debería ser cb en lugar de cd
+    cb(null, './uploads');
   },
   filename: (req, file, cb) => {
     const ext = file.originalname.split('.').pop();
@@ -61,12 +60,13 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send({ data: 'Imagen cargada' });
-});
+const upload =multer({storage})
 
+app.post('/upload',upload.single('file'),(req,res)=>{
+  res.send({data:'Imagen cargada'})
+}
+)
 
 
 // Ruta para la página principal
